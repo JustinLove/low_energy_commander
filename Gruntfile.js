c630f96394b8ebf305aa71b75451de62d7ab1370
Justin Love <git@JustinLove.name>
2014-11-08 13:36:07 -0600

    commander for dynamic energy

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+var spec = require('./lib/spec')
+var prompt = require('prompt')
+prompt.start()
+
+var modPath = '../../server_mods/com.wondible.pa.low_energy_commander/'
+var stream = 'stable'
+var media = require('./lib/path').media(stream)
+
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    copy: {
+      mod: {
+        files: [
+          {
+            src: [
+              'modinfo.json',
+              'LICENSE.txt',
+              'README.md',
+              'CHANGELOG.md',
+              'ui/**',
+              'pa/**'],
+            dest: modPath,
+          },
+        ],
+      },
+    },
+    clean: ['pa', modPath],
+    // copy files from PA, transform, and put into mod
+    proc: {
+      commander: {
+        targets: [
+          'pa/units/commanders/base_commander/base_commander.json'
+        ],
+        process: function(spec) {
+          spec.production.energy = 300
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerMultiTask('proc', 'Process unit files into the mod', function() {
+    if (this.data.targets) {
+      var specs = spec.copyPairs(grunt, this.data.targets, media)
+      spec.copyUnitFiles(grunt, specs, this.data.process)
+    } else {
+      var specs = this.filesSrc.map(function(s) {return grunt.file.readJSON(media + s)})
+      var out = this.data.process.apply(this, specs)
+      grunt.file.write(this.data.dest, JSON.stringify(out, null, 2))
+    }
+  })
+
+  // Default task(s).
+  grunt.registerTask('default', ['proc', 'copy:mod']);
+
+};
+
